5bafae1705796031d9e190a1f7a2e1552750e145
Nirbhai singh <120583283+1995lovely@users.noreply.github.com>
2024-07-30 18:51:40 +0530

    ec2.tf

---
+++ ec2.tf
@@ -0,0 +1,9 @@
+provider "aws" {
+   region = "us-east-1"
+   access_key = ""
+   secret_key = ""
+}
+resource "aws_instance" "myec2instance" {
+    ami= "ami-0427090fd1714168b"
+    instance_type= "t2.micro"
+}
